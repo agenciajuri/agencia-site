758759505b75a8a9e8581cd0467848c8d1b0bb32
airtonsouza <airkun.2014@gmail.com>
2026-02-18 19:16:33 -0300

    workflow githubactions

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    output: 'export',
     images: {
+        unoptimized: true,
         domains: [],
         dangerouslyAllowSVG: true,
         contentDispositionType: 'attachment',
